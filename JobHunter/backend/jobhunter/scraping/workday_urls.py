7f508f6fd0d5b4276cc8cdb16e9673228b6e03db
Shourav Rakshit Ivan <69850843+ShouravRakshit@users.noreply.github.com>
2025-02-23 23:43:57 -0700

    added jobs to the list

---
+++ JobHunter/backend/jobhunter/scraping/workday_urls.py
@@ -0,0 +1,90 @@
+company_urls = [
+    {
+        "company_name": "BDO Canada ",
+        "url": "https://bdo.wd3.myworkdayjobs.com/en-US/BDO/"
+    },
+    {
+        "company_name":"Alberta Health Services",  
+        "url": "https://careers.albertahealthservices.ca/"
+    },
+    {
+        "company_name": "Berkeley Payment Solutions Inc.",
+        "url": "https://jobs.berkeleypayment.com/jobs/Careers"
+    },
+    {
+        "company_name": "Jardeg",
+        "url": "https://jobs.construction-benefits.com/Jardeg-Construction/search/?createNewAlert=false&q=&locationsearch="
+    },
+
+    {
+        "company_name": "Sobeys",
+        "url": "https://jobs.sobeyscareers.com/sitemap.xml"
+    },
+    {
+        "company_name": "AMTRA Solutions",
+        "url": "https://amtrasolutions.com/careers/"
+    },
+    {
+        "company_name": "AMTRA Solutions",
+        "url": "https://www.amtrasolutions.com/careers"
+    },
+    {
+        "company_name":"Keyera",
+        "url":"https://keyera.wd3.myworkdayjobs.com/en-US/Keyera_Careers/"
+    },
+    {
+        "company_name": "Alberta Blue Cross",
+        "url": ""
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+        
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+        
+]
