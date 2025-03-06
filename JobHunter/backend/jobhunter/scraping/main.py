7845e8936a9322131556d355866b2b14ca6e0a5f
Shourav Rakshit Ivan <69850843+ShouravRakshit@users.noreply.github.com>
2025-03-06 09:26:01 -0700

    added project_diagram pics

--- JobHunter/backend/jobhunter/scraping/main.py
+++ JobHunter/backend/jobhunter/scraping/main.py
@@ -7,7 +7,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
 from bs4 import BeautifulSoup
 from workday_urls import company_urls
 
